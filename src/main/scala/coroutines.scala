7c66585fd3de4b423e9607c4873ed45e1f523731
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2011-11-17 17:22:34 +0100

    Adding the coroutine example.

--- src/main/scala/coroutines.scala
+++ src/main/scala/coroutines.scala
@@ -6,21 +6,70 @@ import scala.util.continuations._
 
 object coroutines {
   
+  /* coroutine API */
+  
   trait Coroutine {
     var continuation = () => reset {
-      apply()
+      body()
     }
     
-    def apply(): Unit @suspendable
+    def apply(): Unit = {
+      continuation()
+      /* if you want to make a coroutine spin forever... look below:
+      continuation = () => reset {
+        body()
+      }
+      apply()
+      */
+    }
+    
+    def body(): Unit @suspendable
   }
   
-  /* method used for coroutine declaration */
-  def coroutine(body: () => Unit @suspendable) = new Coroutine {
-    def apply() = body()
+  def yieldTo(from: Coroutine, to: Coroutine): Unit @suspendable = shift {
+    (k: Unit => Unit) =>
+    from.continuation = () => k()
+    to.apply()
+  }
+  
+  def coroutine(thunk: =>Unit @suspendable): Coroutine = new Coroutine {
+    def body() = thunk
+  }
+  
+  /* ----------------------------------------------------------------------- */
+  
+  /* our example */
+  
+  var stack: List[Int] = Nil
+  
+  val producer: Coroutine = coroutine {
+    var i = 0
+    while (i < 10) {
+      println("push")
+      stack ::= i
+      yieldTo(producer, consumer)
+      i += 1
+    }
+  }
+  
+  val consumer: Coroutine = coroutine {
+    while (true) {
+      while (stack.nonEmpty) {
+        println("popping " + stack.head)
+        stack = stack.tail
+      }
+      yieldTo(consumer, producer)
+    }
+  }
+  
+  def coroutineEnvironment(block: =>Unit @suspendable) = reset {
+    block
   }
   
   def main(args: Array[String]) {
-    
+    coroutineEnvironment {
+      producer()
+    }
   }
   
 }
